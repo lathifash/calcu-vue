7f27c8a22143e6d00ea7616f6d54f1ddee48e567
thifa <113702902+lathifash@users.noreply.github.com>
2023-12-18 17:27:10 +0100

    Add files via upload

---
+++ calculator-veu/vue-project/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./components/Calculator.vue";
+
+export default {
+  name: "app",
+  components: {
+    Calculator,
+  },
+};
+</script>
+
+<style>
+html,
+body {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #46b759;
+}
+
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
